f64a711006a945d302abebebbb8508582455c0f6
Sivaramakrishnan Kasiviswanathan <vishnu2503@gmail.com>
2019-09-07 20:52:07 +0530

    Updates

---
+++ precache-manifest.fb39b7c06ddf91ba159f98f087fcb671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda01bdf7fb8c02190cc3ad11b861731",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "be98497a9e3187cdbffc",
+    "url": "/monsters-rolodex/static/css/main.5ed89068.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "be98497a9e3187cdbffc",
+    "url": "/monsters-rolodex/static/js/main.a020cc9b.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
